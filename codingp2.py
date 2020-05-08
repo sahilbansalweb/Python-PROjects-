b2e62422c2bc74e2809ad3ea07c53dcff002f4bc
Sahil-Bansal-prog <sahilbansal1233@gmail.com>
2020-05-08 21:03:13 +0530

    MY submissons are here

--- codingp2.py
+++ codingp2.py
@@ -1,6 +1,9 @@
-for i in range(0,30):
-    print(i)
-print("DOne by sahil bansal")
-print("its git practice actually")
-print("Branch wali pracitce")
-print('new change in branch1')
+def Quick_Sort(lista):
+    if len(lista) <= 1:
+        return lista
+    key = lista[0]
+    equal = [x for x in lista if x == key]
+    minor = [x for x in lista if x < key]
+    major = [x for x in lista if x > key]
+    return Quick_Sort(minor) + equal + Quick_Sort(major)
+
