6add8add169aa426282a6d7bfc88b1654ddb2471
Sahil-Bansal-prog <sahilbansal1233@gmail.com>
2020-05-10 22:55:30 +0530

    nothing

---
+++ Project4.py
@@ -0,0 +1,12 @@
+"""
+|---------------------------------------------<  MINI PROJECT => 4  >--------------------------------------------------\
+|------------------------------------<  Generating a sine vs cosine curve  >-------------------------------------------\
+"""
+from matplotlib import pyplot as plt
+x = [5,2,7]
+y = [2,16,4]
+plt.plot(x,y)
+plt.title('Info')
+plt.ylabel('Y axis')
+plt.xlabel('X axis')
+plt.show()
